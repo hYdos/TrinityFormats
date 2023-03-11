3e356aa93a222816c5c6816d58398049ed1db0db
hydos <25501603+hYdos@users.noreply.github.com>
2023-03-11 14:29:26 +1000

    Some more Gui Work Might come back to it at another time

--- src/main/java/me/hydos/trinityutils/gui/GuiApplication.java
+++ src/main/java/me/hydos/trinityutils/gui/GuiApplication.java
@@ -3,26 +3,53 @@ package me.hydos.trinityutils.gui;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.control.cell.CheckBoxTreeCell;
+import javafx.scene.control.cell.TextFieldTreeCell;
 import javafx.stage.Stage;
-import me.hydos.trinityutils.model.skeleton.TrinitySkeleton;
-import me.hydos.trinityutils.util.FlatCWrapper;
-import me.hydos.trinityutils.util.GenericModel;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class GuiTestsUntilWifi extends Application{
+public class GuiApplication extends Application {
     private static final Path TRSKL_SCHEMA = Paths.get("C:/Users/allegra/Documents/PokeDocs-main/SV/Flatbuffers/model/trskl.fbs");
     private static final Path TRANM_SCHEMA = Paths.get("C:/Users/allegra/Documents/PokeDocs-main/SV/Flatbuffers/animation/tranm.fbs");
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        var loader = new FXMLLoader(GuiTestsUntilWifi.class.getResource("/gui.fxml"));
+        var loader = new FXMLLoader(GuiApplication.class.getResource("/gui.fxml"));
         var scene = new Scene(loader.load());
         primaryStage.setTitle("Trinity Explorer");
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        var dir = "C:\\Users\\allegra\\Documents\\Github\\hYdos\\PokeFileTools\\src\\main\\java";
+        var treeView = (TreeView<String>) scene.lookup("#fileTree");
+        // Creates the root item.
+        var rootItem = new TreeItem<>("file.glb");
+
+        treeView.setCellFactory(TextFieldTreeCell.forTreeView());
+
+        createTree(Paths.get(dir), rootItem);
+        treeView.setRoot(rootItem);
+        System.out.println("ok");
+    }
+
+    public static void createTree(Path file, TreeItem<String> parent) {
+        if (Files.isDirectory(file)) {
+            var treeItem = new TreeItem<>(file.getFileName().toString());
+            parent.getChildren().add(treeItem);
+            try {
+                for (var f : Files.list(file).toList()) {
+                    createTree(f, treeItem);
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else parent.getChildren().add(new TreeItem<>(file.getFileName().toString()));
     }
 
     public static void main(String[] args) {
